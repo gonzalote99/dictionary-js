4760586164d6783f4f137e7219ffac39b581fec9
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2022-08-16 17:45:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+var xhttp = new XMLHttpRequest();
+
+function strUcFirst(a){return (a+'').charAt(0).toLocaleUpperCase()+a.substr(1);}
+
+function clearSearch() {
+  if(document.getElementById("searchForm").value == "") {
+    var search = "color";
+    loadSearch(search);
+  }
+}
+
+function loadSearch(search) {
+ if (search == "search") {
+   var search = document.getElementById("searchForm").value;
+
+ }
+ var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
+
+ xhttp.open("GET", url, true);
+ xhttp.send();
+ var imgUrl = "https://source.unsplash.com/featured/?" + search;
+ document.getElementById("searchImage").innerHTML = "<img src='"+imgUrl+"' crossorigin='anonymous'></img>";   
+}
+
+xhttp.onreadystatechange = function() {
+  if(xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("definition").innerHTML = " ";
+    var response = JSON.parse(xhttp.responseText);
+    document.getElementById("name").innerHTML = strUcFirst(response["0"].word);
+    i = 0;
+    while (i != response[0].meanings[0].definitions.length) {
+      document.getElementById("definition").innerHTML = "<div id='definitions' class='definition"+ i +"'>"+strUcFirst(response['0']['meanings']['0']['definitions'][i].definition)+"</div>"
+      i += 1;
+    }
+
+
+
+  }
+}
